6695fed5506ea3a98960f1b00e89e22a79ab7b2f
Nitesh <niteshkumar899935@gmail.com>
2024-11-10 18:10:03 +0530

    setup configured

--- src/app.js
+++ src/app.js
@@ -15,5 +15,12 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+//routes
+
+import userRouter from "./routes/user.routes.js";
+
+// routes declarartion
+
+app.use("/api/v1/users", userRouter)
 
 export {app}
